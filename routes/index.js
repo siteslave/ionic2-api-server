3938823c4ee28f6a44597423aaae948114f14eb8
Satit Rianpit <rianpit@gmail.com>
2016-09-16 02:15:24 +0700

    add jwt

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 'use strict'
 
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var express = require('express');
 var router = express.Router();
 
+let secretKey = 'abcdefghijklmn123456'
+ 
 router.post('/login', (req, res, next) => {
   let db = req.db;
   console.log(req.body)
@@ -19,7 +22,13 @@ router.post('/login', (req, res, next) => {
     })
     .then((rows) => {
       if (rows.length) {
-        res.send({ ok: true, username: rows[0].username })
+        let token = jwt.sign({username: username}, secretKey, {
+          expiresIn: "2 days"
+        })
+
+        console.log(token);
+
+        res.send({ ok: true, token: token })
       } else {
         res.send({ ok: false, msg: 'Invalid Username/Password!' })
       }
@@ -28,6 +37,24 @@ router.post('/login', (req, res, next) => {
       console.log(err)
       res.send({ ok: false, msg: err })
     });
+})
+
+router.get('/products', (req, res, next) => {
+  let token = req.query.token;
+  let db = req.db;
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      res.send({ok: false, msg: 'Invalid token'})
+    } else {
+      console.log(decoded);
+      db('products')
+        .orderBy('name')
+        .then(rows => res.send({ ok: true, rows: rows }))
+        .catch(err => res.send({ ok: false, msg: err }));
+    }
+  });
+
 })
 
 module.exports = router;
